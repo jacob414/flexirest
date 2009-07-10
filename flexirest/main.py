24b55f66bd2e7eba1049b52f44ceaf85584615ec
jacob@medea <jacob@medea>
2009-07-10 15:34:21 +0200

    Quick konceptual sketch

--- flexirest/main.py
+++ flexirest/main.py
@@ -5,7 +5,21 @@ import optparse
 from flexirest import meta
 from flexirest.util import StdoutConsole
 
+from docutils.parsers.rst import roles
+from docutils.core import publish_parts
+
 GLOBAL_USAGE = 'flexirest <options>'
+
+DEFAULT_TEMPLATE = u"""%(html_prolog)s
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+%(html_head)s
+</head>
+<body>
+%(html_body)s
+</body>
+</html>
+"""
 
 def commandline(args=None, console=None):
     """
@@ -24,9 +38,31 @@ def commandline(args=None, console=None):
                       dest='version',
                       default=False,
                       help='print version and exit')
+    parser.add_option('-t',
+                      '--template',
+                      dest='template'
+                      default=None,
+                      help='apply source into this template')
+    parser.add_option('-r',
+                      '--roles',
+                      dest='template'
+                      default=None,
+                      help='apply source into this template')
+
     options, args = parser.parse_args(args)
     if options.version:
         console.write("flexirest version '%s'" % meta.VERSION)
         return 0
+    if options.template is None:
+        template = DEFAULT_TEMPLATE
+    if options.roles is not None:
+        try:
+            roles_mod = __import__(option.roles)
+        except ImportError:
+                pass
+
+        role_names = (role[5:] for role in
+                      dir(__import__(options.roles)) if role.startswith('role_'))
+
 
     return 0
