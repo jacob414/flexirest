03702f5d5e1d091d5db651eac2f671145eb7e0fd
jacob@medea <jacob@medea>
2009-09-18 17:23:25 +0200

    Reflected new raising behaviour of _import and new integration tests directory structure

--- test/test_main_integration.py
+++ test/test_main_integration.py
@@ -6,7 +6,7 @@ from docutils import nodes
 
 from nose.tools import assert_equals, assert_true, with_setup, raises
 
-from .. import support
+import support
 
 from flexirest import main
 
@@ -28,7 +28,7 @@ the_template %(whole)s
 @with_setup(test_tmpl_basic_creator, support.clean_testfiles)
 def test_template_basic():
     capture = StringIO()
-    main._import = imp.new_module
+    main._import = lambda m, r: imp.new_module(m)
     rc = main.commandline(['--template=%s' % BASIC_TMPL, '--writer=pseudoxml'],
                           source=MINIMAL_FIXTURE, destination=capture)
     out = capture.getvalue()
@@ -57,7 +57,7 @@ def test_full_role():
 
     rolesmod = imp.new_module('roles')
     rolesmod.role_foo = role_foo
-    main._import = lambda n: rolesmod
+    main._import = lambda m, r: rolesmod
     capture = StringIO()
     rc = main.commandline(['--template=%s' % ROLE_TMPL, '--writer=pseudoxml'],
                           source=fullrole_src, destination=capture)
