4b86c19abfa1bd3e51bf89a8874581955a16355e
jacob@medea <jacob@medea>
2009-11-12 16:53:30 +0100

    Validate generated PDF contents with pyPdf

--- flexirest/test/test_tex.py
+++ flexirest/test/test_tex.py
@@ -9,6 +9,7 @@ from StringIO import StringIO
 from nose.tools import assert_equals, assert_true, with_setup
 
 from flexirest.tex import *
+from flexirest.test import support
 
 import support
 
@@ -19,14 +20,22 @@ def write_fake_style(path):
         \endinput
         """))
 
+def sanity_check_pdf(pdf):
+    assert_equals(pdf[:8], '%PDF-1.4')
+    assert_equals(pdf[-6:], '%%EOF\n')
+
+def extract_pdf_text(pdf):
+    return support.pdf_from_data(pdf).getPage(0).extractText()
+
 def test_simplest():
     pdf = latex2pdf(textwrap.dedent(ur"""
     \documentclass{article}
     \begin{document}
     Simplest
     \end{document}"""))
-    assert_equals(pdf[:8], '%PDF-1.4')
-    assert_equals(pdf[-6:], '%%EOF\n')
+    sanity_check_pdf(pdf)
+    text = extract_pdf_text(pdf)
+    assert_true(text.startswith(u'Simplest'))
 
 def test_utf8_encoded():
     # Not using raw unicode string here, see
@@ -39,31 +48,31 @@ def test_utf8_encoded():
     Å ä ö
     \\end{document}
     """))
-    assert_equals(pdf[:8], '%PDF-1.4')
-    assert_equals(pdf[-6:], '%%EOF\n')
+    sanity_check_pdf(pdf)
+    text = extract_pdf_text(pdf)
+    assert_true(text.startswith(u'Aao')) # pdflatex quirkiness, but correct
 
-_styles_dir = None
+_styles_dir = []
+
+def latex_tmp(name):
+    return os.path.join(_styles_dir[0], name)
 
 def setup_styles_dir():
-    global _styles_dir
-    _styles_dir = tempfile.mkdtemp(prefix='fr-test-w-style-')
+    _styles_dir.append(tempfile.mkdtemp(prefix='fr-test-w-style-'))
+    write_fake_style(latex_tmp('flexifake.sty'))
 
 def teardown_styles_dir():
-    global _styles_dir
-    shutil.rmtree(_styles_dir)
+    shutil.rmtree(_styles_dir[0])
 
 @with_setup(setup_styles_dir, teardown_styles_dir)
 def test_w_style():
-    global _styles_dir
-    stylepath = os.path.join(_styles_dir, 'flexifake.sty')
-    write_fake_style(stylepath)
-
     pdf = latex2pdf(textwrap.dedent("""
     \\documentclass{article}
     \\usepackage{flexifake}
     \\begin{document}
     Uses package flexifake.
-    \\end{document}"""), (stylepath,))
-    assert_equals(pdf[:8], '%PDF-1.4')
-    assert_equals(pdf[-6:], '%%EOF\n')
+    \\end{document}"""), (latex_tmp('flexifake.sty'),))
+    sanity_check_pdf(pdf)
+    text = extract_pdf_text(pdf)
+    assert_true(text.startswith(u'Usespackage'))
 
