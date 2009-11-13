65db644497587fb3cf7cae9a9ac447ba7943cf3a
jacob@medea <jacob@medea>
2009-11-13 14:24:01 +0100

    Code freeze: updated version number to 0.8.2

--- flexirest/meta.py
+++ flexirest/meta.py
@@ -1,6 +1,6 @@
 SHORT_NAME='flexirest'
 CMDLINE_DESC = 'The medium-featured, flexible reStructuredText utility'
-VERSION = '0.8.1dev'
+VERSION = '0.8.2'
 URL = 'http://www.aspektratio.net/flexirest'
 AUTHOR = 'Jacob Oscarson'
 EMAIL = 'jacob@aspektratio.net'
