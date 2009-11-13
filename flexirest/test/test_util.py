acc77d4f2d90f92de70a6c9e3c5d216fa474699f
jacob@medea <jacob@medea>
2009-11-13 12:14:10 +0100

    Implemented a buffered file object

--- flexirest/test/test_util.py
+++ flexirest/test/test_util.py
@@ -2,7 +2,7 @@ import os
 
 from StringIO import StringIO
 
-from nose.tools import assert_equals
+from nose.tools import assert_equals, assert_false, with_setup
 
 from flexirest import util
 
@@ -15,3 +15,20 @@ def test_StdoutConsoleWrite():
 def test_Duck():
     d = util.Duck(ze_attr='ze attr')
     assert_equals(d.ze_attr, 'ze attr')
+
+
+class TestBufferedFile(object):
+
+    # XXX Implementation drawback: platform dependant
+    @with_setup(lambda: None, lambda: os.unlink('/tmp/write.this'))
+    def test_write(self):
+        bf = util.BufferedFile('/tmp/write.this')
+        bf.write('what was written')
+        bf.close()
+        assert_equals(open('/tmp/write.this', 'r').read(), 'what was written')
+
+    # XXX Implementation drawback: platform dependant
+    def test_no_write(self):
+        bf = util.BufferedFile('/tmp/dont.write.this')
+        bf.close()
+        assert_false(os.path.exists('/tmp/dont.write.this'))
--- flexirest/util.py
+++ flexirest/util.py
@@ -1,7 +1,16 @@
+from __future__ import with_statement
+
 import os
 import sys
+from cStringIO import StringIO
+
+__docformat__ = 'reStructuredText'
 
 class StdoutConsole(object):
+    """
+    Vaguely file-like object that immediately writes it output to
+    `sys.stdout` followed by an os-appropriate `EOL`.
+    """
 
     def __init__(self):
         self.out = sys.stdout
@@ -22,3 +31,32 @@ class Duck(object):
         a = ', '.join('%s=%r' % i for i in self.__dict__.items())
         return '<%s object at 0x%x%s%s>' % (
                 type(self).__name__, id(self), ': ' if a else '', a)
+
+class BufferedFile(file):
+    """
+    File-like object that writes to an internal buffer. It will only
+    write to the file system when it's `.close()` method is called
+    **and** it has actually written something to itself.
+
+    Inspired by `tempfile.SpooledTemporaryFile`, but with far less
+    features (yagni.. for now it only supports writing, for example).
+    """
+
+    def __init__(self, path):
+        self._buf = StringIO()
+        self.path = path
+        # Make shure that we seem open and functional..
+        super(BufferedFile, self).__init__(os.devnull, 'wb')
+
+    def write(self, data):
+        self._buf.write(data)
+
+    def flush(self):
+        curdata = self._buf.getvalue()
+        if len(curdata) > 0:
+            with open(self.path, 'wb') as fp:
+                fp.write(curdata)
+
+    def close(self):
+        self.flush()
+        super(BufferedFile, self).close()
