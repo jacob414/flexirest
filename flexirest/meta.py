2b4b1f1dc14f172249d805134cb85ec928d56686
jacob@medea <jacob@medea>
2009-11-17 11:21:46 +0100

    Proper way of marking for development, keyword expansion didn't work

--- flexirest/meta.py
+++ flexirest/meta.py
@@ -1,9 +1,8 @@
 SHORT_NAME='flexirest'
 CMDLINE_DESC = 'The medium-featured, flexible reStructuredText utility'
-VERSION = '0.8.3dev'
+VERSION = '0.8.3'
 URL = 'http://www.aspektratio.net/flexirest'
 AUTHOR = 'Jacob Oscarson'
 EMAIL = 'jacob@aspektratio.net'
 SHORT_DESC = CMDLINE_DESC
 CMDLINE_USAGE = 'flexirest <options>'
-HG_REVISION = '$Revision:$'
