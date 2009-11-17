95e57d6043050141c044f9306ea0aee4944d004e
jacob@medea <jacob@medea>
2009-11-17 10:54:17 +0100

    Markup meta.py for 0.8.4 dev, experiment with keyword expansion

--- flexirest/meta.py
+++ flexirest/meta.py
@@ -1,8 +1,9 @@
 SHORT_NAME='flexirest'
 CMDLINE_DESC = 'The medium-featured, flexible reStructuredText utility'
-VERSION = '0.8.2'
+VERSION = '0.8.3dev'
 URL = 'http://www.aspektratio.net/flexirest'
 AUTHOR = 'Jacob Oscarson'
 EMAIL = 'jacob@aspektratio.net'
 SHORT_DESC = CMDLINE_DESC
 CMDLINE_USAGE = 'flexirest <options>'
+HG_REVISION = '$Revision:$'
