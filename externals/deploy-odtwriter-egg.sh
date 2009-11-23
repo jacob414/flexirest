475410079784cffa1040d7940585dd3ab828cd76
jacob@medea <jacob@medea>
2009-11-23 18:55:42 +0100

    Check out fresh copies of OdtWriter from svn when deploying eggified version

--- externals/deploy-odtwriter-egg.sh
+++ externals/deploy-odtwriter-egg.sh
@@ -3,15 +3,10 @@
 # XXX Just a proof-of-concept, this really ought to be a buildout task
 # XXX of some kind
 
-FLEXIREST_REPO=~/src/flexirest
-SANDBOX=~/src/ext/docutils-sandbox-svn
-
-cd $SANDBOX/OpenDocument
-python setup.py sdist
-cd $SANDBOX/OpenDocument/dist/
-tar zxf odtwriter-1.3d.tar.gz
-cd $SANDBOX/OpenDocument/dist/odtwriter-1.3d
-patch -p0 < $FLEXIREST_REPO/externals/odtwriter-eggpatch.diff
+rm -rf odtwriter-svn
+svn co svn://svn.berlios.de/docutils/trunk/sandbox/OpenDocument odtwriter-svn
+cd odtwriter-svn
+patch -p0 < ../odtwriter-eggpatch.diff
 python setup.py bdist_egg
 python2.5 setup.py bdist_egg
 rsync -r dist/*.egg flexisite:~/public_html/flexirest/dist/
