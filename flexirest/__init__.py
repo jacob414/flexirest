3571932489173b116de2f06f28e582996308215f
Jacob Oscarson <jacob@plexical.com>
2012-01-05 15:49:29 +0100

    Meta info can just as well sit in the __init__.py file

--- flexirest/__init__.py
+++ flexirest/__init__.py
@@ -1,1 +1,26 @@
 __docformat__ = 'reStructuredText'
+
+SHORT_NAME='flexirest'
+CMDLINE_DESC = 'The flexible and friendly reStructuredText utility'
+VERSION = '0.9dev'
+URL = 'http://www.aspektratio.net/flexirest'
+AUTHOR = 'Jacob Oscarson'
+EMAIL = 'jacob@plexical.com'
+SHORT_DESC = CMDLINE_DESC
+CMDLINE_USAGE = 'flexirest <options>'
+INFO = """Flexirest
+Flexible and friendly reStructuredText renderer
+Version %s
+
+Usage: flexirest <writer name> [options] [infile] [outfile]
+"""
+STATUS = """The following writers are functional in your installation:
+{{for line in functional}}
+  {{line}}
+{{endfor}}
+
+The following writers are not functional in your installation:
+{{for line in nonfunctional}}
+  {{line}}
+{{endfor}}
+"""
