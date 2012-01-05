1648cfd559c704c52044947f41a129cbf17878f0
Jacob Oscarson <jacob@plexical.com>
2012-01-05 15:50:54 +0100

    There are better stuff nowadays than Buildout

---
+++ pavement.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""pavement.py -- pavement for Flexirest.
+
+Copyright 2011 Jacob Oscarson. See LICENCE for permissions.
+"""
+import os
+import sys
+
+from paver.easy import *
+from paver.setuputils import setup
+
+import flexirest
+
+setup(
+    name=flexirest.SHORT_NAME,
+    packages=(flexirest.SHORT_NAME),
+    version='0.9dev',
+    author=flexirest.AUTHOR,
+    author_email=flexirest.EMAIL,
+    install_requires=open(os.path.join('deps',
+                                       'requirements.txt')).readlines()
+)
+
+@task
+def readme():
+    "Builds top-level README.rst"
+
+@task
+def virtualenv():
+    "Prepares a checked out directory for development"
+    if not os.path.exists(os.path.join('bin', 'pip')):
+        sys.path.insert(0, os.path.join('deps', 'virtualenv.zip'))
+        import virtualenv
+        virtualenv.create_environment('.')
+    else:
+        print('Virtualenv already set up')
+
+@needs('virtualenv')
+@task
+def env():
+    "Ensure virtualenv exists and is up to date"
+    sh('./bin/pip install -r deps/requirements.txt')
+    sh('./bin/pip install -r deps/development.txt')
+
+@task
+def clean():
+    "Remove everything created by the build process"
+    path('bin').rmtree()
+    path('lib').rmtree()
+    path('include').rmtree()
--- setup.py
+++ setup.py
@@ -1,35 +1,10 @@
+import sys
 import os
-from setuptools import setup, find_packages
 
-from flexirest import meta
+try:
+    import paver
+except ImportError:
+    sys.path.insert(0, os.path.join('deps', 'paver-minilib.zip'))
 
-packages = [meta.SHORT_NAME]
-
-setup(name=meta.SHORT_NAME,
-      packages=packages,
-      version=meta.VERSION,
-      author=meta.AUTHOR,
-      author_email=meta.EMAIL,
-      url=meta.URL,
-      description=meta.SHORT_DESC,
-      long_description=open('docs/introduction.txt', 'r').read(),
-      install_requires=['docutils'],
-      entry_points = {
-          'console_scripts': (
-              'flexirest = flexirest.main:commandline',
-              )
-          },
-      classifiers = [
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Environment :: Console',
-          'Topic :: Documentation',
-          'Topic :: Software Development :: Documentation',
-          'Topic :: Text Processing :: Markup',
-          ],
-      license='GNU LGPL',
-      test_suite='nose.collector')
-
+import paver.tasks
+paver.tasks.main()
